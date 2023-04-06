af11f21499917d82b5a3c6701365367f3513ac30
Wissame_ <76851329+W1554M3@users.noreply.github.com>
2023-04-06 15:18:12 +0700

    final

---
+++ Midterm/net-framework-main/IUP-BMI Calculator/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using IUP_BMI_Calculator.Repository;
+
+namespace IUP_BMI_Calculator;
+
+public partial class App : Application
+{
+	public BMIResultRepository BMIResultRepository;
+	public App(string dbPath)
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+
+		BMIResultRepository._dbPath = dbPath;
+		BMIResultRepository.conn = new SQLite.SQLiteConnection(dbPath);
+	}
+}
